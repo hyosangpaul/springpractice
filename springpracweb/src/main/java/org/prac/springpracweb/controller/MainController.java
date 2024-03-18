d95308359faa88ffe40cbc4184abec2bccc55fd3
PAUL <paul950104@gmail.com>
2024-03-18 16:32:56 +0900

    new setting

---
+++ springpracweb/src/main/java/org/prac/springpracweb/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.prac.springpracweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main(Model model) {
+        return "main";
+    }
+}
---
+++ springpracweb/src/test/java/org/prac/springpracweb/SpringpracwebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prac.springpracweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpracwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
