16db04313f6b63128efc7018d44612c2c075a6ba
PAUL <paul950104@gmail.com>
2024-03-18 16:33:22 +0900

    new setting

---
+++ springpracweb/src/main/java/org/prac/springpracweb/controller/SpringpracwebApplication.java
@@ -0,0 +1,13 @@
+package org.prac.springpracweb.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpracwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpracwebApplication.class, args);
+    }
+
+}
